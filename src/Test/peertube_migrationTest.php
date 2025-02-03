890ab05c7493097d3142bf3ad0773f16e067f3d1
emv38 <emv38@pitt.edu>
2025-02-03 12:59:55 -0500

    formatting button with configformbase

--- src/Test/peertube_migrationTest.php
+++ src/Test/peertube_migrationTest.php
@@ -7,18 +7,57 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\peertube_migration\peertube_migration_session;
 
 
-class peertube_migrationTest {
+class peertube_migrationTest extends ConfigFormBase {
 
-    function peertube_migration_test_form ($form, &$form_state) {
-        $form['button'] = array(
-            '#type' => 'test',
-            '#value' => 'test peertube api call',
-            '#submit' => array('peertube_test_api')
-        );
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'peertube_migration_test_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'peertube_migration.test_form',
+    ];
+  }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm ($form, FormStateInterface $form_state) {
+    $form['button'] = array(
+        '#type' => 'test',
+        '#value' => 'test peertube api call',
+        '#submit' => array('::peertube_test_api')
+    );
+    // return $form;
+    return parent::buildForm($form, $form_state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+    // run the test call from here
+    parent::submitForm($form, $form_state);
     }
     
-    function peertube_test_api($form, &$form_state) {
+    function peertube_test_api($form, FromStateInterface $form_state) {
         // test api call 
+        // create new session to test connection
+        $session = new peertube_migration_session();
+
+        try {
+            // try logging and getting ID
+            $session_id = $session->getSession();
+            \Drupal::logger('peertube_migration')->notice('API SUCCESS!! session ID: ' . $session_id);
+        } catch (\Exception $e) {
+            \Drupal::logger('peertube_migration')->notice('Issue retrieving session id with error: ' . $e);
+        }
     }
 
 }
