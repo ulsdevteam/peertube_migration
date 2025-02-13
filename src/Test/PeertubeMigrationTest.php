58a9eb626763bb97acba2050745d35e38bd07017
emv38 <emv38@pitt.edu>
2025-02-13 13:03:24 -0500

    pulling title from node id

--- src/Test/PeertubeMigrationTest.php
+++ src/Test/PeertubeMigrationTest.php
@@ -96,7 +96,7 @@ class PeertubeMigrationTest extends ConfigFormBase {
 
       $constant_object = $storage->loadMultiple($query);
 
-      \Drupal::messenger()->addMessage('query response to node 16581: ' . $constant_object, 'status');
+      \Drupal::messenger()->addMessage('query response to node 16581: ' . $constant_object[16581]['entityKeys']['label'], 'status');
 
       $response = $session->request('GET' , "/api/v1/videos/$video_id/captions");
       // $data = $response->getBody()->getContents();
