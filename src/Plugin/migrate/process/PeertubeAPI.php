ccf1e2dcfe856a9dbfb17234e51e92bcf9047b44
emv38 <emv38@pitt.edu>
2025-02-26 15:42:51 -0500

    first part of migration -- copying file over

--- src/Plugin/migrate/process/PeertubeAPI.php
+++ src/Plugin/migrate/process/PeertubeAPI.php
@@ -33,11 +33,19 @@ class PeertubeAPI extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    //full url placed into process plugin: https://peertube-dev-01.library.pitt.edu/w/7YA55ipVYPydKPdETbMZfJ
+    // only need id after the w/
+
+    preg_match('/w\/([a-zA-Z0-9]+)/', $value, $matches);
+    $video_id = $matches[1];
+    
+
     // make a request to peertube API
     $session = \Drupal::service('peertube_migration.peertube_migration_session');
 
     //get video caption link
-    $response = $session->request('GET' , "/api/v1/videos/$value/captions");
+    $response = $session->request('GET' , "/api/v1/videos/$video_id/captions");
 
     // return ending of caption path
     return $response['data'][0]['captionPath'];
